58798e47ae5a19396fb27a04cf799692086526d7
StefaniaVitali <stefy.egypt@hotmail.it>
2024-04-08 14:10:10 +0200

    ADD: select style for invalid selection

--- js/app.js
+++ js/app.js
@@ -22,17 +22,28 @@ formElement.addEventListener('submit', function(event){
     let prezzoOrario = 0
     //--SE il valore è uguale a 1 il prezzo orario 20.50
     if(lavoro.value === "1"){
+       lavoro.classList.remove('is-invalid')
+       document.getElementById('select-validation').innerHTML =""
+       document.getElementById('select-validation').classList.remove('invalid-feedback')
        prezzoOrario = 20.50
      //-- ALTRIMENTI SE il valore è uguale a 2 il prezzo orario 15.30   
     } else if (lavoro.value === "2"){
+       lavoro.classList.remove('is-invalid')
+       document.getElementById('select-validation').innerHTML =""
+       document.getElementById('select-validation').classList.remove('invalid-feedback') 
        prezzoOrario = 15.30
     //-- ALTRIMENTI SE il valore è uguale a 3 il prezzo orario 33.60    
     } else if (lavoro.value === "3"){
+       lavoro.classList.remove('is-invalid')
+       document.getElementById('select-validation').innerHTML =""
+       document.getElementById('select-validation').classList.remove('invalid-feedback')
        prezzoOrario = 33.60
     //-- ALTRIMENTI non è possibile calcolare il preventivo  
     } else {
-        console.log('error!, non hai selezionato nessun lavoro') //==> bisogna sistemarlo
-    }
+       lavoro.classList.add('is-invalid')
+       document.getElementById('select-validation').innerHTML =`Non hai selezionato nessuna tipolagia di lavoro valida: non è possibile calcolare il tuo preventivo`
+       document.getElementById('select-validation').classList.add('invalid-feedback')
+      }
     //calcola il prezzo base 
     const prezzoBase = oreLavoro * prezzoOrario
     console.log(prezzoBase) 
@@ -58,21 +69,18 @@ formElement.addEventListener('submit', function(event){
       document.getElementById('promo-validation').innerHTML = `Il codice inserito non è valido`
       document.getElementById('promo-validation').classList.add('invalid-feedback')
       
-    }
-
-
-   
+    }   
 
     const prezzoFinale = prezzoBase - sconto
     console.log(prezzoFinale, sconto)  
     
     //--stampare il prezzo in forma umana
     let boldPrice = prezzoFinale.toFixed(2).split('');
-    console.log(boldPrice);
+    // console.log(boldPrice);
     let nonBoldPrice = boldPrice.splice(-3);
     let pointDecimal = nonBoldPrice.splice(0,1,",")
-    console.log(pointDecimal, nonBoldPrice)
-    console.log(boldPrice)  
+    // console.log(pointDecimal, nonBoldPrice)
+    // console.log(boldPrice)  
     
     paragraphElement.innerHTML = `<span class="h3 fw-bolder"> &euro; ${boldPrice.join('')}</span><span class="h4 fw-light">${nonBoldPrice.join('')}</span>`
 });
